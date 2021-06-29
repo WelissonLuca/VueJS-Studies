f1979dd36c1f2fe6910334fc1ee722bf7d631e63
WelissonLuca <welissonluca17@gmail.com>
2021-06-29 15:32:37 -0300

    feat: firs_project: configuration prettier

--- firs_project_vue/src/App.vue
+++ firs_project_vue/src/App.vue
@@ -1,14 +1,8 @@
 <template>
-  <h2> Hello World</h2>
+  <h2>Hello World</h2>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-
-
-export default defineComponent({
-  
-});
+import { defineComponent } from 'vue';
+export default defineComponent({});
 </script>
-
-
